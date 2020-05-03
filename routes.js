69224c0fc8208bf2af05f8c963f11344f3a0cc12
Rachelik <rachel.ik.87@gmail.com>
2020-05-03 23:49:18 +0800

    some changes

--- routes.js
+++ routes.js
@@ -9,13 +9,12 @@ module.exports = (app, allModels) => {
   const notbookCtrl = require('./controllers/notbook')(allModels);
 
   app.get('/category/new', notbookCtrl.newCategory)
-
   app.get('/category', notbookCtrl.category);
-
+  app.post('/category', notbookCtrl.updateCategory);
   // app.get('/welcome', notbookCtrl.welcome)
   // app.get('/category', notbookCtrl.category);
-  // app.get('/category/new', notbookCtrl.cats)
-  // app.post('/category', notbookCtrl.updateCategory);
+
+
 
   // app.get('/category/:id', notbookCtrl.showCategory);
 
